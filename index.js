c2acb85be0fb71b49b42f744893df0547ecd9911
Developer Davo <DeveloperDavo@users.noreply.github.com>
2020-01-28 18:03:27 +0100

    Add index.html and index.js
    Fetch reviews

---
+++ index.js
@@ -0,0 +1,13 @@
+function reviews() {
+  fetch(
+    'https://api-qa.trustedshops.com/rest/internal/v2/shops/X6A4AACCD2C75E430381B2E1C4CLASSIC/reviews.json'
+  )
+    .then(response => {
+      return response.json()
+    })
+    .then(myJson => {
+      console.log(myJson)
+    })
+}
+
+reviews()
